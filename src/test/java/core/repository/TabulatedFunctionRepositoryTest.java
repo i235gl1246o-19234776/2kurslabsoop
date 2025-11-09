550cebbaab25f021c68634603cc6ca9b13f8aadf
Владислав Николаев <eshteben@email.com>
2025-11-09 18:11:04 +0400

    изменены тесты для репозиториев

--- src/test/java/core/repository/TabulatedFunctionRepositoryTest.java
+++ src/test/java/core/repository/TabulatedFunctionRepositoryTest.java
@@ -5,13 +5,18 @@ import core.entity.TabulatedFunctionEntity;
 import core.entity.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
         "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class TabulatedFunctionRepositoryTest {
--- src/test/java/core/repository/UserRepositoryTest.java
+++ src/test/java/core/repository/UserRepositoryTest.java
@@ -3,13 +3,18 @@ package core.repository;
 import core.entity.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
         "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class UserRepositoryTest {
@@ -30,7 +35,7 @@ class UserRepositoryTest {
 
         assertThat(saved).isNotNull();
         assertThat(found).isNotNull();
-        assertThat(found.getName()).isEqualTo("testuser");
+        assertThat(found.getName()).isEqualTo("tester");
         assertThat(deleted).isNull();
     }
 }
